cd38f6eb26a6a84b7da2d34228fcddc753875003
seb-art <kimanzikims300@gmail.com>
2024-03-16 22:13:20 +0300

    implemented auth0 authentication

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,14 +1,18 @@
-import React from "react";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function MainNav() {
+  const { loginWithRedirect } = useAuth0();
   return (
     <div>
       <Button
         variant="ghost"
         className="
         ont-bold hover:text-green-500 hover:bg-white"
-      >Sign In</Button>
+        onClick={async () => await loginWithRedirect()}
+      >
+        Sign In
+      </Button>
     </div>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import './global.css'
-import AppRoutes from './AppRoutes'
+import "./global.css";
+import AppRoutes from "./AppRoutes";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <AppRoutes />
+      <Auth0ProviderWithNavigate>
+        <AppRoutes />
+      </Auth0ProviderWithNavigate>
     </Router>
   </React.StrictMode>
-)
+);
