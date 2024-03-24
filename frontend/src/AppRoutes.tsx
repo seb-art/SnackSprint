893618d9da09015c4eea6e5379cb14e1efaaf0bd
seb-art <kimanzikims300@gmail.com>
2024-03-24 22:24:36 +0300

    Acess Token

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './layouts/layout'
 import HomePage from './pages/HomePage'
+import AuthCallbackPage from './pages/AuthCallbackPage'
 
 function AppRoutes () {
   return (
     <>
       <Routes>
         <Route path='/' element={<Layout><HomePage /></Layout>} />
+        <Route path='/auth-callback' element={<AuthCallbackPage/>} />
         <Route path='/user-profile' element={<span>USER PROFILE PAGE</span>} />
         <Route path='*' element={<Navigate to='/' />} />
       </Routes>
