a9ad9cf95bb1855ffba3c8c3b8da1bc946115ea6
seb-art <kimanzikims300@gmail.com>
2024-04-15 23:19:03 +0300

    preparing for 1st deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,21 +3,27 @@ import cors from "cors";
 import "dotenv/config";
 
 import mongoose from "mongoose";
-import myUserRoute from "./routes/MyUserRoute"
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
-    .then(() => { console.log("connected to database") })
-    .catch((error) => {
-        console.log("There was a problem connecting to the database", error)
-    })
+import myUserRoute from "./routes/MyUserRoute";
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((error) => {
+    console.log("There was a problem connecting to the database", error);
+  });
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
+
 app.use("/api/my/user", myUserRoute);
 
-
 app.listen(7000, () => {
-    console.log("server started on localhost:7000");
+  console.log("server started on localhost:7000");
 });
